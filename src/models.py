f27dd6cfdaa604ad503fc6f7d6cccd383dd9e515
deepu <deeputvm1992@gmail.com>
2021-02-11 10:12:06 +0530

    adds network architecture

---
+++ src/models.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 10 08:42:10 2021
+
+@author: deepu
+"""
+
+import torch.nn as nn
+
+
+
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        
+        DROPOUT = 0.5
+
+        # 59049 x 1
+        self.conv1 = nn.Sequential(
+            nn.Conv1d(9, 128, kernel_size=3, stride=3, padding=0),
+            nn.BatchNorm1d(128),
+            nn.ReLU())
+        # 19683 x 128
+        self.conv2 = nn.Sequential(
+            nn.Conv1d(128, 128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm1d(128),
+            nn.ReLU(),
+            nn.MaxPool1d(3, stride=1))
+        # 6561 x 128
+        self.conv3 = nn.Sequential(
+            nn.Conv1d(128, 128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm1d(128),
+            nn.ReLU(),
+            nn.MaxPool1d(3,stride=1))
+        # 2187 x 128
+        self.conv4 = nn.Sequential(
+            nn.Conv1d(128, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm1d(256),
+            nn.ReLU(),
+            nn.MaxPool1d(3,stride=1))
+        # 729 x 256
+        self.conv5 = nn.Sequential(
+            nn.Conv1d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm1d(256),
+            nn.ReLU(),
+            nn.MaxPool1d(3,stride=1))
+        # 243 x 256
+        self.conv6 = nn.Sequential(
+            nn.Conv1d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm1d(256),
+            nn.ReLU(),
+            nn.MaxPool1d(3,stride=1),
+            nn.Dropout(DROPOUT))
+        # 81 x 256
+        self.conv7 = nn.Sequential(
+            nn.Conv1d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm1d(256),
+            nn.ReLU(),
+            nn.MaxPool1d(3,stride=1))
+        # 27 x 256
+        self.conv8 = nn.Sequential(
+            nn.Conv1d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm1d(256),
+            nn.ReLU(),
+            nn.MaxPool1d(3,stride=1))
+        # 9 x 256
+        self.conv9 = nn.Sequential(
+            nn.Conv1d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm1d(256),
+            nn.ReLU(),
+            nn.MaxPool1d(3,stride=1))
+        # 3 x 256
+        self.conv10 = nn.Sequential(
+            nn.Conv1d(256, 512, kernel_size=3, stride=1, padding=0),
+            nn.BatchNorm1d(512),
+            nn.ReLU(),
+            nn.MaxPool1d(3,stride=1))
+        # 1 x 512 
+        self.conv11 = nn.Sequential(
+            nn.Conv1d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm1d(512),
+            nn.ReLU(),
+            nn.Dropout(DROPOUT))
+        # 1 x 512 
+        self.fc = nn.Linear(11264, 50)
+        self.final_layer = nn.Linear(50, 6)
+    def forward(self, x):
+        # input x : 23 x 59049 x 1
+        # expected conv1d input : minibatch_size x num_channel x width
+
+        #x = x.view(x.shape[0], 1,-1)
+        # x : 23 x 1 x 59049
+
+        out = self.conv1(x)
+        out = self.conv2(out)
+        out = self.conv3(out)
+        out = self.conv4(out)
+        out = self.conv5(out)
+        out = self.conv6(out)
+        out = self.conv7(out)
+        out = self.conv8(out)
+        out = self.conv9(out)
+        out = self.conv10(out)
+        out = self.conv11(out) 
+        
+        #print(out.shape)
+        #print(x.shape)
+        out = out.view(x.shape[0],-1)
+        #print(out.shape)
+        out = self.fc(out)
+        return self.final_layer(out)
